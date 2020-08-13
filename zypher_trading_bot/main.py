2d96ec759db2ea9631ffbb14f3af5e1643f81f82
unknown <sebastian.oderland@gmail.com>
2020-08-13 11:58:44 +0200

    update

--- zypher_trading_bot/main.py
+++ zypher_trading_bot/main.py
@@ -2,5 +2,7 @@ from ib_insync import IB, util
 from ib_insync.contract import *
 
 
-def run():
-    pass
+def run(contracts, ip="127.0.0.1", port=7496):
+    ib = IB()
+    ib.connect(ip, port, clientId=1)
+
