20d3cd3c6446d28014e5d15b8ba7f7929792cf22
SebastianOderland <sebastian.oderland@gmail>
2020-08-13 12:37:25 +0200

    update

--- auto.sh
+++ auto.sh
@@ -4,16 +4,12 @@ git config --global user.email sebastian.oderland@gmail
 git config --global user.name SebastianOderland
 git config --global github.token 7eada1464f1ef7f528ffe4c070aaf43b5c5a6178
 
-git add .
-read -p "Commit description: " desc
-git commit -m "$desc"
-git push https://SebastianOderland:Ferabulok7568@github.com/SebastianOderland/zypher_trading_bot.git master
-
-read -p "Version: " version
-
 branch=$(git rev-parse --abbrev-ref HEAD)
 repo_full_name=$(git config --get remote.origin.url | sed 's/.*:\/\/github.com\///;s/.git$//')
 token=$(git config --global github.token)
+
+read -p "Commit description: " desc
+read -p "Version: " version
 
 generate_post_data()
 {
@@ -22,13 +18,18 @@ generate_post_data()
   "tag_name": "$version",
   "target_commitish": "$branch",
   "name": "$version",
-  "body": "",
+  "body": "null",
   "draft": false,
   "prerelease": false
 }
 EOF
 }
-echo "https://api.github.com/repos/$repo_full_name/releases?access_token=$token"
+
+git add .
+
+git commit -m "$desc"
+git push "https://SebastianOderland:Ferabulok7568@github.com/$repo_full_name.git master"
+
 curl --data "$(generate_post_data)" "https://api.github.com/repos/$repo_full_name/releases?access_token=$token"
 
 #python setup.py sdist
