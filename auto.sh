6452f5bddb5a563bd27ed817b54ca4b25d0f5398
SebastianOderland <sebastian.oderland@gmail>
2020-08-13 12:39:14 +0200

    update

--- auto.sh
+++ auto.sh
@@ -2,7 +2,7 @@
 
 git config --global user.email sebastian.oderland@gmail
 git config --global user.name SebastianOderland
-git config --global github.token 7eada1464f1ef7f528ffe4c070aaf43b5c5a6178
+git config --global github.token 802f80d7640739421cf7b57db9126985d2a1ff71
 
 branch=$(git rev-parse --abbrev-ref HEAD)
 repo_full_name=$(git config --get remote.origin.url | sed 's/.*:\/\/github.com\///;s/.git$//')
