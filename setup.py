4b11e0b2c4f306a9fb3c9e79549c60d151e5cc26
SebastianOderland <sebastian.oderland@gmail>
2020-08-13 17:47:57 +0200

    update

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from distutils.core import setup
 setup(
     name='zypher_trading_bot',  # How you named your package folder (MyLib)
     packages=['zypher_trading_bot'],  # Chose the same as 'name'
-    version='1.1.2',  # Start with a small number and increase it with every change you make
+    version='1.1.3',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Trading Bot',  # Give a short description about your library
     author='Sebastian Oderland',  # Type in your name
     author_email='sebastian.oderland@oderland.se',  # Type in your E-Mail
     url='https://github.com/SebastianOderland/zypher_trading_bot',  # Provide either the link to your github or to your website
     download_url='https://github.com/SebastianOderland/zypher_trading_bot/releases/tag/v_'
-    + '1.1.2'
+    + '1.1.3'
     + '.tar.gz',  # I explain this later on
     keywords=['zypher',],  # Keywords that define your package best
     install_requires=['ib_insync',],
