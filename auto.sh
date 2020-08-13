8d0fd4567b489daeaf1b4f784bc253e89a0e5f55
SebastianOderland <sebastian.oderland@gmail>
2020-08-13 15:46:57 +0200

    update

--- auto.sh
+++ auto.sh
@@ -32,4 +32,4 @@ git push origin master
 curl --data "$(generate_post_data)" "https://api.github.com/repos/SebastianOderland/zypher_trading_bot/releases?access_token=$token"
 
 python3 setup.py sdist "$version"
-twine upload dist/*
+c:/python38/lib/site-packages/twine upload dist/*
