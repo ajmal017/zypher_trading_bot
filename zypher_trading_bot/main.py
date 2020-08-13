1375c1b6700a67ed40c57a6b9791408d71374ee2
SebastianOderland <sebastian.oderland@gmail.com>
2020-08-13 21:45:51 +0200

    update

--- zypher_trading_bot/main.py
+++ zypher_trading_bot/main.py
@@ -9,4 +9,10 @@ def run(strategy, **kwargs):
 
 
 if __name__ == "__main__":
-    run(TestStrategy, data0="EUR.USD-CASH-IDEALPRO", port=7497)
+    run(
+        TestStrategy,
+        data0="EUR.USD-CASH-IDEALPRO",
+        port=7497,
+        timeframe="Minutes",
+        compression=5,
+    )
