0a91d8f1f0bdf79288446f17e30de41f3cb3ce4c
SebastianOderland <sebastian.oderland@gmail>
2020-08-13 12:23:30 +0200

    update

--- auto.sh
+++ auto.sh
@@ -2,15 +2,12 @@
 
 git config --global user.email sebastian.oderland@gmail
 git config --global user.name SebastianOderland
-git config --global github.user SebastianOderland
-git config --global github.pass Ferabulok7568
-git config --global github.passwd Ferabulok7568
-git config --global github.password Ferabulok7568
 
+GIT_PASSWORD="Ferabulok7568"
 git add .
 read -p "Commit description: " desc
 git commit -m "$desc"
-echo "SebastianOderland" | git push origin master
+git push origin master
 
 #read -p "Version: " version
 #git tag "v_$version"
