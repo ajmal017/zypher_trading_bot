b3434afb7725fb41c67db89b090474f7972ae11a
SebastianOderland <sebastian.oderland@gmail>
2020-08-13 12:34:52 +0200

    update

--- auto.sh
+++ auto.sh
@@ -4,7 +4,6 @@ git config --global user.email sebastian.oderland@gmail
 git config --global user.name SebastianOderland
 git config --global github.token 7eada1464f1ef7f528ffe4c070aaf43b5c5a6178
 
-GIT_PASSWORD="Ferabulok7568"
 git add .
 read -p "Commit description: " desc
 git commit -m "$desc"
@@ -29,7 +28,7 @@ generate_post_data()
 }
 EOF
 }
-
+echo "https://api.github.com/repos/$repo_full_name/releases?access_token=$token"
 curl --data "$(generate_post_data)" "https://api.github.com/repos/$repo_full_name/releases?access_token=$token"
 
 #python setup.py sdist
