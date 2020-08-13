3dd42d77c344b4b69c68f0cddf71da8fbfc704f5
SebastianOderland <sebastian.oderland@gmail>
2020-08-13 15:17:54 +0200

    update

--- auto.sh
+++ auto.sh
@@ -39,6 +39,12 @@ git push origin master
 #   https://api.github.com/app/installations/287237342/access_tokens \
 #   -d '{"repository_ids":[287237342]}'
 
+curl \
+  -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/"$repo_full_name"/releases \
+  -d '$(generate_post_data)'
+
 # curl --data "$(generate_post_data)" "https://api.github.com/repos/$repo_full_name/releases?access_token=$token"
 
 
